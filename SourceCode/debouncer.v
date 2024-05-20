8de605a20ebb07cb68b7e7bb5af7da65712db5b7
faridaasaidd <125366174+faridaasaidd@users.noreply.github.com>
2024-05-20 09:32:29 +0300

    Create debouncer.v

---
+++ SourceCode/debouncer.v
@@ -0,0 +1,19 @@
+`timescale 1ns / 1ps
+
+
+module debouncer(input clk, rst, in, output out);
+reg q1,q2,q3;
+always@(posedge clk, posedge rst) begin
+ if(rst == 1'b1) begin
+ q1 <= 0;
+ q2 <= 0;
+ q3 <= 0;
+ end
+ else begin
+ q1 <= in;
+ q2 <= q1;
+ q3 <= q2;
+ end
+end
+assign out = (rst) ? 0 : q1&q2&q3;
+endmodule
